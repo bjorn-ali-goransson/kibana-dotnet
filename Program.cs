66428b5acb07f21d15d5e19b18b4723f0972bfb2
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2025-03-14 06:54:50 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
+using Elastic.Apm.NetCoreAll;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure services
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+// Configure APM
+app.UseAllElasticApm(builder.Configuration);
+
+// Configure the HTTP request pipeline
+app.UseRouting();
 
 app.MapGet("/", () => "Hello from .NET API!");
 
